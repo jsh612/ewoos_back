83cbd0fe98965b191459f7066ee0ba17f00acee2
seungho Jeon <urijsh612@gmail.com>
2020-03-27 15:50:02 +0900

    [수정] 특정 상품 이미 대여중일 경우 에러처리

--- src/api/Rent/ReqRent/ReqRent.resolvers.ts
+++ src/api/Rent/ReqRent/ReqRent.resolvers.ts
@@ -1,5 +1,5 @@
 import { IResolvers } from "graphql-tools";
-import { prisma } from "../../../../generated/prisma-client";
+import { prisma } from "./../../../../generated/prisma-client/index";
 import { ReqRentMutationArgs, ReqRentResponse } from "./../../../types/graph.d";
 
 const resolvers: IResolvers = {
@@ -13,6 +13,19 @@ const resolvers: IResolvers = {
       const { postId, message } = args;
       const { user } = request;
       try {
+        const existingRent = await prisma.rents({
+          where: {
+            post: {
+              id: postId
+            }
+          }
+        });
+        if (existingRent.length !== 0) {
+          return {
+            ok: false,
+            error: "이미 대여중 입니다."
+          };
+        }
         await prisma.createRent({
           post: {
             connect: { id: postId }
